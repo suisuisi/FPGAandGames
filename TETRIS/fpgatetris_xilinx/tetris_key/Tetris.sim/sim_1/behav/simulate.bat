4e8509dafddc985434ce6cc63a251310162659ed
openfpga <10243450+openfpga@user.noreply.gitee.com>
2022-05-15 22:52:49 +0800

    恢复

--- TETRIS/fpgatetris_xilinx/tetris_key/Tetris.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\Xilinx\\Vivado\\2014.4\\bin
-call %xv_path%/xsim sim_behav -key {Behavioral:sim_1:Functional:sim} -tclbatch sim.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
